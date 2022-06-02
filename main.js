8247616e27312eb379d8c2fb34f364fb47573b53
FelpFS1 <phff800@gmail.com>
2022-06-02 19:52:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    document.querySelector('.menu').classList.toggle('menu_toggle');
+    document.querySelector('.hamburguer').classList.toggle('colorB')
+    document.querySelector('.check').children[0].classList.toggle('color')
+    document.querySelector('.check').children[2].classList.toggle('color')  
+}
