2307b21accb57a635b1cf099d104b9daf5a83da9
unknown <phff800@gmail.com>
2024-01-09 09:50:32 -0300

    update for projects

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 - Esse projeto de portifolio foi feito para adicionar os meus projetos em forma visual!
 
+## Tecnologias usadas:
+
+- HTML
+- CSS
+- JavaScript
+
+## Desenvolvido por:
+
+- Felipe Farias
+
--- js/showGif.js
+++ js/showGif.js
@@ -1,16 +1,46 @@
-document.querySelectorAll(".project-img-wrapper").forEach((e) => {
-    e.addEventListener("click", (e) => {
-      const className = e.currentTarget.className
-      if(className.includes('image1')){
-          const pai = e.currentTarget.parentNode
-      const dados = pai.innerHTML
-       console.log(dados);
-       pai.classList.add('show-gif')
-       pai.innerHTML = ""
-      }else{
-          console.log("nao e a img 1");
+function showGif(e) {
+  e.addEventListener("click", (e) => {
+    
+    const className = e.currentTarget.className;
+    const div = e.currentTarget.parentNode;
+    const dados = div.cloneNode(true);
+    const classItem = [
+      {
+        0: { name: "image1", class: "show-gif" },
+      },
+    ];
+    classItem.forEach((item, i) => {
+      if (className.includes(item[i].name)) {
+        div.classList.add(item[i].class);
+        div.innerHTML = ""
+        createButton(item,i,div,dados);
       }
-       
-      });
-  })
+      
+    });
+  });
+}
+
+document.querySelectorAll(".project-img-wrapper").forEach((e) => {
+  showGif(e);
+});
+
+
+function createButton(item, i, div, data) {
+    const button = document.createElement("button");
+    button.id = "cancel-gif";
+    button.textContent = "X";
+    div.appendChild(button);
   
+    button.addEventListener('click', function cancelGif() {
+      div.classList.remove(item[i].class);
+      div.parentNode.replaceChild(data, div);
+  
+     
+      document.querySelectorAll(".project-img-wrapper").forEach((e) => {
+        showGif(e);
+      });
+      
+    });
+  }
+
+
